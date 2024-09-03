8c6deceacac98783b0071d69cb84c3ba4331bccb
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-03 12:23:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,19 @@ from streamlit_echarts import st_echarts
 
 # Configuración de la página
 st.set_page_config(page_title="NLPMatchJobs", layout="wide")
+
+st.markdown("""
+    <style>
+        .reportview-container {
+            margin-top: -2em;
+        }
+        #MainMenu {visibility: hidden;}
+        .stDeployButton {display:none;}
+        footer {visibility: hidden;}
+        #stDecoration {display:none;}
+    </style>
+""", unsafe_allow_html=True)
+
 # Apply custom theme
 st.markdown("""
     <style>
