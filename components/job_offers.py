e21201135bcbc81961f358501016d2cec094cd09
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-05 00:59:01 +0200

    Update job_offers.py

--- components/job_offers.py
+++ components/job_offers.py
@@ -94,6 +94,6 @@ def display_job_offers(data):
             with st.expander("Ubicación donde se realiza el trabajo"):
                 st.markdown(f"""
                 <div class="card-content">
-                    <p>{offer['Localidad']}, {offer['Provincia']}</p>
+                    <p>{offer['Localidad']}</p>
                 </div>
                 """, unsafe_allow_html=True)
