0240af79faa2f658c39e7143d613047797d2e1fa
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-04 17:16:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ from nltk.corpus import stopwords
 import nltk
 from components.comparison import get_gpt_explanation
 from components.job_offers import display_job_offers
+from components.job_offers import display_candidates
 from google_sheets import read_sheet
 from utills.visualization import display_bar_chart
 from streamlit_echarts import st_echarts
