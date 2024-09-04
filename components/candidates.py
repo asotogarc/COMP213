92fbfea9609c4ad336cdca0bf82ca1fb52dee61a
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-04 17:57:02 +0200

    Update candidates.py

--- components/candidates.py
+++ components/candidates.py
@@ -4,17 +4,20 @@ import streamlit as st
 def display_candidates(data):
     if 'candidates' not in st.session_state:
         st.session_state.candidates = data.sample(n=min(3, len(data))).to_dict('records')
-    
+        
     n_samples = len(st.session_state.candidates)
     if n_samples == 0:
         st.warning("No hay candidatos para mostrar.")
         return None
 
-    st.markdown('<h2 class="section-title">CANDIDATOS</h2>', unsafe_allow_html=True)
-
+    st.markdown('CANDIDATOS', unsafe_allow_html=True)
     cols = st.columns(n_samples)
+
     for i, (candidate, col) in enumerate(zip(st.session_state.candidates, cols)):
-       
+        with col:
+            # Verificar si el candidato está seleccionado
+            is_selected = st.session_state.get('selected_candidate') == candidate
+
             # Botón centrado
             col1, col2, col3 = st.columns([1,2,1])
             with col2:
@@ -24,18 +27,19 @@ def display_candidates(data):
                     else:
                         st.session_state.selected_candidate = candidate
                     st.rerun()
-
-                            # Expander para los conocimientos
-            with st.expander("Ver Conocimientos"):
-                    st.markdown(f"""
-                    <h3>Conocimientos</h3>
-                    <p>{candidate['Conocimientos']}</p>
-                    """, unsafe_allow_html=True)
-
-            with st.expander("Ver Experiencia"):
-                    st.markdown(f"""
-                    <h3>Conocimientos</h3>
-                    <p>{candidate['Experiencia']}</p>
-                    """, unsafe_allow_html=True)
-
             
+            # Expander para los conocimientos
+            with st.expander("Ver Conocimientos"):
+                st.markdown(f"""
+                Conocimientos
+                
+                {candidate['Conocimientos']}
+                """, unsafe_allow_html=True)
+
+            # Expander para la experiencia
+            with st.expander("Ver Experiencia"):
+                st.markdown(f"""
+                Experiencia
+                
+                {candidate['Experiencia']}
+                """, unsafe_allow_html=True)
