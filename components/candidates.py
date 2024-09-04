afe8754806aeb08c386e39c473d7f83df8afdba6
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-04 18:44:08 +0200

    Update candidates.py

--- components/candidates.py
+++ components/candidates.py
@@ -64,7 +64,7 @@ def display_candidates(data):
                 st.markdown(f"""
                 
                 
-                {{candidate['Localidad'],candidate['Provincia']}}
+                {candidate['Localidad'],candidate['Provincia']}
                 """, unsafe_allow_html=True)
 
                         # Expander para la experiencia
