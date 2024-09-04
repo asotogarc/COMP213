e74d5c661e5cb459c16ee4d4960e4e33e3a036b2
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-04 19:02:03 +0200

    Update candidates.py

--- components/candidates.py
+++ components/candidates.py
@@ -16,7 +16,7 @@ def display_candidates(data):
             # Verificar si el candidato está seleccionado
             is_selected = st.session_state.get('selected_candidate') == candidate
 
-            with st.expander("Ver Idiomas"):
+            with st.expander("Ver Nombre"):
                 st.markdown(f"""{candidate['Nombre']}""", unsafe_allow_html=True)
             # Botón centrado
             col1, col2, col3 = st.columns([1, 2, 1])
