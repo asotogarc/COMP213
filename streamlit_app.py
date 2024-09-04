d6b9d8f4e9b2f33f61b0309c8d7430ee31ec6a11
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-04 13:17:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,6 @@ from components.candidates import display_candidates
 from google_sheets import read_sheet
 from utills.visualization import display_bar_chart
 from streamlit_echarts import st_echarts
-import go
 
 # Configuración de la página
 st.set_page_config(page_title="NLPMatchJobs", layout="wide")
